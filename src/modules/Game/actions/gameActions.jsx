357f569caef4032af22eec4371269b8f72480d7d
Briskie,Tyler <tyler.briskie@vertafore.com>
2018-05-07 12:39:44 -0600

    added reducer and actions for game component

---
+++ src/modules/Game/actions/gameActions.jsx
@@ -0,0 +1,12 @@
+// export const SHUFFLE_DECK = 'SHUFFLE_DECK';
+export const NEW_GAME = 'NEW_GAME';
+export const HAND_OVER = 'HAND_OVER';
+export const ADD_PLAYER = 'ADD_PLAYER';
+// export const REMOVE_PLAYER = 'REMOVE_PLAYER';
+
+
+export const newGameAction = () => ({ type: NEW_GAME });
+export const handOverAction = () => ({ type: HAND_OVER });
+export const newPlayerAction = () => ({ type: ADD_PLAYER });
+// export const removePlayerAction = () => ({ type: REMOVE_PLAYER });
+
