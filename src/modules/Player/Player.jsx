748de6aa18b676e558d1f8a6e6e207f2eb1a32c4
Tyler Briskie <tylerbriskie@gmail.com>
2018-07-28 14:48:19 -0600

    Added wager buttons to player component

--- src/modules/Player/Player.jsx
+++ src/modules/Player/Player.jsx
@@ -12,8 +12,15 @@ class Player extends React.Component {
         console.log("PLAYER PROPS", this.props)
         return (
             <div className="player-object">
-                <h3>{this.props.name}</h3>
+                <div className="player-header">
+                    <h3>{this.props.name}</h3>
+                    <h3>{this.props.bankRoll}</h3>
+                </div>
 
+                <div className="player-buttons">
+                    <button className="wager-button">-</button>
+                    <button className="wager-button">+</button>
+                </div>
             </div>
         )
     }
