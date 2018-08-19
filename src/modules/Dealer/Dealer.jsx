7bf8c7f977f3c2a9651d8799807be8330fcf94e2
Tyler Briskie <tylerbriskie@gmail.com>
2018-08-18 20:30:47 -0600

    scss rename

--- src/modules/Dealer/Dealer.jsx
+++ src/modules/Dealer/Dealer.jsx
@@ -2,40 +2,6 @@ import React from 'react';
 import './Dealer.css';
 const Dealer = (props) => {
 
-        //
-        // const buildHands = () => {
-        //     return this.props.hands.map((hand) => {
-        //         const cards = hand.cards.map(card => {
-        //             return (<div className="card-container"><img className={`single-card`} src={card.image} /></div>)
-        //         });
-        //         const hardValue = hand.hardValue;
-        //         const softValue = hand.softValue;
-        //         const isBusted = hand.isBusted;
-        //         return (
-        //             <div className="player-single-hand">
-        //                 {isBusted ?
-        //                     (
-        //                         <div className="hand=values"> <h1>BUST</h1></div>
-        //                     ) :
-        //                     softValue !== hardValue && softValue < 22 ?
-        //                         (<div className="hand-values">
-        //                                 <h1>{hardValue}</h1>
-        //                                 <h1>(soft {softValue})</h1>
-        //                             </div>
-        //                         )
-        //                         :
-        //                         <div className="hand-values">
-        //                             <h1>{hardValue}</h1>
-        //                         </div>
-        //                 }
-        //                 <div className="hand-cards">
-        //                     {cards}
-        //                 </div>
-        //
-        //
-        //             </div>)
-        //     });
-        // };
     const dealerCards = props.cards.map(card => {
         return (<div className="card-container"><img className={`single-card`} src={card.image} /></div>)
     });
